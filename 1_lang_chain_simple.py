ce506fd797d46c44a28764894e1c0642d1dfb372
alexbioinf <ayudin@bioxceltherapeutics.com>
2023-06-13 17:33:05 +0200

    Agents

--- 1_lang_chain_simple.py
+++ 1_lang_chain_simple.py
@@ -5,6 +5,7 @@ https://medium.com/@avra42/getting-started-with-langchain-a-powerful-tool-for-wo
 
 """
 import openai
+import os
 from langchain.chains import LLMChain, SimpleSequentialChain  # import LangChain libraries
 from langchain.llms import OpenAI  # import OpenAI model
 from langchain.prompts import PromptTemplate # import PromptTemplate
@@ -12,7 +13,7 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 
 
-llm = OpenAI(model_name="text-davinci-003", max_tokens=300, temperature=0.9, openai_api_key=config['OPENAI_API_KEY'])
+llm = OpenAI(model_name="text-davinci-003", max_tokens=300, temperature=0.9, openai_api_key=os.getenv('OPENAI_API_KEY'))
 
 user_question = "Cyanobacteria can perform photosynthetsis, are they considered as plants?"
 
